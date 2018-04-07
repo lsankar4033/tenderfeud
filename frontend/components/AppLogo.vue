506e08c7372391ac99406bc768ff949713a7bfbf
reid <reidsherman@gmail.com>
2018-04-06 23:54:59 -0700

    Add frontend

---
+++ frontend/components/AppLogo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button class="button">Button</button>
+  </div>
+</template>
+<script>
+import Test from './Test'
+export default {
+  name: 'AppLogo',
+  components: {
+    Test,
+  }
+
+}
+</script>
+
+<style>
+
+</style>
