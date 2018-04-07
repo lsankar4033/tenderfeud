98fe731a1311f522087fe6da2c4b1258f1eb6bb2
lsankar4033 <lsankar4033@gmail.com>
2018-04-06 23:42:21 -0700

    Really rough (untested) structure for create handler

--- src/polls.js
+++ src/polls.js
@@ -1,11 +1,42 @@
+// Interactions to support:
+// - create/vote on polls
+// - view poll information
+// - become a validator (deposit tokens)
+// - send tokens
+
+let secp256k1 = require('secp256k1')
+
+let { stringify } = require('lotion/lib/json.js')
+
+let { createHash } = require('crypto')
+
+// TODO: Use this on app initialization
+const initialState = {
+  balances: {},
+  polls: {}
+}
+
+function verifyTx(txHash, pubkey, sig) {
+  return secp256k1.verify(sigHash, signature, pubkey);
+}
 
 
+function sha256(data) {
+  return createHash('sha256').update(data).digest()
+}
+
+
+// Perhaps pull this into its own file that client-side (incl frontend) code uses as well
+function txHash(tx) {
+  // TODO
+}
 
 
 // TODO: Options (i.e. initial coinholders, min end block height, parameters)
 function polls () {
 
-  let minEndBlockHeight = 500;
+  const minBlockDuration = 500;
+  const defaultMinAnswers = 2;
 
   // State in words:
   // - pubkey -> balance
@@ -15,41 +46,76 @@ function polls () {
   // State schema
   // {
   //  balances: {pubkey}
-  //  polls: {
-  //    startBlock
-  //    endBlock
-  //    active (redundant)
-  //    pollId: {
-  //      answer: [sorted_pubkeys]
-  //    }
+  //  activePolls: {
+  //    questionHash: {
+  //      startBlock
+  //      endBlock
+  //      question
+  //      answers: {answer: [sorted_pubkeys]}
+  //   }
+  //  }
+  //  inactivePolls: {
+  //    [
+  //      {
+  //        question
+  //        answers: {answer: [sorted_pubkeys]}
+  //        startBlock
+  //        endBlock
+  //      }
+  //    ]
   //  }
   // }
 
   // Create TX Schema
   // question
-  // endBlockHeight
-  // amountToPutUp
-  // creatorAddress
+  // blockDuration
+  // payout
+  // creatorPubkey
   // txSignature (signed by creator privkey)
-  // msgHash
   // (optional) minAnswers
 
-  // Create handling validation rules
-  // - creator matches signature
-  // - creator has amountToPutUp to put up
-  // - min end block height > N
-  // - minAnswers > 2
-
+  // TODO: tests!
   function createHandler(state, tx, chain) {
+    let pubkey = tx.creatorPubkey;
+    let txHash = txHash(tx);
 
+    // Validate signature
+    if (!verifyTx(txHash, pubkey, tx.signature)) {
+      throw Error(`Signature does not match public key!`)
+    }
 
+    // Validate balance
+    let creatorBalance = state.balances[pubkey] || 0;
+    if (creatorBalance < tx.payout) {
+      throw Error(`Creator's balance of ${creatorBalance} not enough to pay payout ${tx.payout}`);
+    }
 
+    // Validate end block height
+    if (tx.blockDuration < minBlockDuration) {
+      throw Error(`Must specify an end block height > ${minBlockDuration}`);
+    }
+
+    // Validate num answers
+    let minAnswers = tx.minAnswers || defaultMinAnswers;
+    if (minAnswers < 2) {
+      throw Error(`Must specify min answers > 2`);
+    }
+
+    // Make sure poll doesn't already exist
+    let questionHash = sha256(tx.question)
+    if (questionHash in state.activePolls) {
+      throw Error(`Can't create a poll idential to a currently active poll!`);
+    }
+
+    // TODO: Perhaps extract into own method for cleanliness
+    let endBlock = chain.height + tx.endBlockHeight
+    state.activePolls[questionHash] = {
+      startBlock: tx.startBlock,
+      endBlock: tx.endBlock,
+      question: tx.question,
+      answers: {}
+    }
   }
 
 }
 
-processCreate = (createTx) => {
-
-  // validate tx (i.e. make sure sender key
-
-}
