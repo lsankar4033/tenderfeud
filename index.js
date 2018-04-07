a280e9e60d50a05413ec9145de6269b9cac27319
lsankar4033 <lsankar4033@gmail.com>
2018-04-07 00:07:35 -0700

    Dummy index.js method

---
+++ index.js
@@ -0,0 +1,10 @@
+let lotion = require('lotion');
+
+// TODO: Use the initial state defined on polls module
+let app = lotion({ initialState: {} })
+
+app.use((state, tx) => {
+  // TODO: Dispatch logic!
+})
+
+app.listen(3000)
