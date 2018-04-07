5513c2a9f97f81c57a1d59e47c690e0830edf93c
Margarita Miranda <marg624@gmail.com>
2018-04-07 01:40:46 -0700

    adding test client

---
+++ client_test.js
@@ -0,0 +1,25 @@
+
+let { post } = require('axios')
+let secp = require('secp256k1')
+let { sha256, pubkeyToAddress, getTxHash, getSignature } = require('./src/utils.js')
+
+let priv = sha256('lakshman')
+let pub = secp.publicKeyCreate(priv)
+
+async function main () {
+  let tx = {
+    type: "create",
+    question: "What is the best cryptocurrency?",
+    blockDuration: 500,
+    payout: 10,
+    creatorPubkey: pub,
+  }
+
+  // sign tx
+  let sigHash = getTxHash(tx)
+  tx.signature = getSignature(sigHash, priv)
+
+  let res = await post('http://localhost:3000/txs', tx)
+  console.log(res.data)
+}
+main()
