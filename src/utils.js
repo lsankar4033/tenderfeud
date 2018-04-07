98ed80d3d26a50a92f56c201370ac77c13d8120c
lsankar4033 <lsankar4033@gmail.com>
2018-04-07 00:53:05 -0700

    pubkeyToAddress

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 let { createHash } = require('crypto')
 let secp256k1 = require('secp256k1')
+let base58check = require('bs58check')
 
 let { stringify } = require('lotion/lib/json.js')
 
@@ -14,6 +15,15 @@ function verifyTx(txHash, pubkey, sig) {
 
 function sha256(data) {
   return createHash('sha256').update(data).digest()
+}
+
+function ripemd160(data) {
+  return createHash('ripemd160').update(data).digest()
+}
+
+function pubkeyToAddress(pubkey) {
+  let hash = ripemd160(sha256(data))
+  return base58check.encode(hash)
 }
 
 // NOTE: This is used on client-side and app-side, so must now know about signature
