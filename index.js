0bbf7bbcaa4ad1df4993058b7f066fd77a68acf9
lsankar4033 <lsankar4033@gmail.com>
2018-04-07 00:22:47 -0700

    Refactoring handlers

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let lotion = require('lotion');
 
-// TODO: Use the initial state defined on polls module
-let app = lotion({ initialState: {} })
+// TODO: Pass in opts
+let polls = require('polls')({});
 
-app.use((state, tx) => {
-  // TODO: Dispatch logic!
-})
+// TODO: Use the initial state defined on polls module
+let app = lotion({ initialState: polls.initialState })
+
+app.use(polls.txHandler)
+app.useBlock(polls.blockHandler)
 
 app.listen(3000)
--- src/polls.js
+++ src/polls.js
@@ -1,16 +1,4 @@
-// Interactions to support:
-// - create/vote on polls
-// - view poll information
-// - become a validator (deposit tokens)
-// - send tokens
-
 let { verifyTx, sha256, getTxHash } = require('./utils.js');
-
-// TODO: Use this on app initialization
-const initialState = {
-  balances: {},
-  polls: {}
-}
 
 // State schema
 // {
@@ -36,12 +24,7 @@ const initialState = {
 // }
 
 
-// TODO: Options (i.e. initial coinholders, min end block height, parameters)
-function polls () {
-
-  const minBlockDuration = 500;
-  const defaultMinAnswers = 2;
-
+function createHandler(state, tx, chain) {
   // Create TX Schema
   // question
   // blockDuration
@@ -50,48 +33,79 @@ function polls () {
   // txSignature (signed by creator privkey)
   // (optional) minAnswers
 
-  // TODO: tests!
-  function createHandler(state, tx, chain) {
-    let pubkey = tx.creatorPubkey;
-    let txHash = getTxHash(tx);
+  let pubkey = tx.creatorPubkey;
+  let txHash = getTxHash(tx);
 
-    // Validate signature
-    if (!verifyTx(txHash, pubkey, tx.signature)) {
-      throw Error(`Signature does not match public key!`)
-    }
-
-    // Validate balance
-    let creatorBalance = state.balances[pubkey] || 0;
-    if (creatorBalance < tx.payout) {
-      throw Error(`Creator's balance of ${creatorBalance} not enough to pay payout ${tx.payout}`);
-    }
-
-    // Validate end block height
-    if (tx.blockDuration < minBlockDuration) {
-      throw Error(`Must specify an end block height > ${minBlockDuration}`);
-    }
-
-    // Validate num answers
-    let minAnswers = tx.minAnswers || defaultMinAnswers;
-    if (minAnswers < 2) {
-      throw Error(`Must specify min answers > 2`);
-    }
-
-    // Make sure poll doesn't already exist
-    let questionHash = sha256(tx.question)
-    if (questionHash in state.activePolls) {
-      throw Error(`Can't create a poll idential to a currently active poll!`);
-    }
-
-    // TODO: Perhaps extract into own method for cleanliness
-    let endBlock = chain.height + tx.endBlockHeight
-    state.activePolls[questionHash] = {
-      startBlock: tx.startBlock,
-      endBlock: tx.endBlock,
-      question: tx.question,
-      answers: {}
-    }
+  // Validate signature
+  if (!verifyTx(txHash, pubkey, tx.signature)) {
+    throw Error(`Signature does not match public key!`)
   }
 
+  // Validate balance
+  let creatorBalance = state.balances[pubkey] || 0;
+  if (creatorBalance < tx.payout) {
+    throw Error(`Creator's balance of ${creatorBalance} not enough to pay payout ${tx.payout}`);
+  }
+
+  // Validate end block height
+  if (tx.blockDuration < minBlockDuration) {
+    throw Error(`Must specify an end block height > ${minBlockDuration}`);
+  }
+
+  // Validate num answers
+  let minAnswers = tx.minAnswers || defaultMinAnswers;
+  if (minAnswers < 2) {
+    throw Error(`Must specify min answers > 2`);
+  }
+
+  // Make sure poll doesn't already exist
+  let questionHash = sha256(tx.question)
+  if (questionHash in state.activePolls) {
+    throw Error(`Can't create a poll idential to a currently active poll!`);
+  }
+
+  // TODO: Perhaps extract into own method for cleanliness
+  let endBlock = chain.height + tx.endBlockHeight
+  state.activePolls[questionHash] = {
+    startBlock: tx.startBlock,
+    endBlock: tx.endBlock,
+    question: tx.question,
+    answers: {}
+  }
 }
 
+
+function blockHandler(state, chain) {
+  // TODO: Implement!
+}
+
+// TODO: Options (i.e. initial coinholders, min end block height, parameters)
+function polls (opts) {
+  const minBlockDuration = 500;
+  const defaultMinAnswers = 2;
+
+  const initialState = {
+    balances: {},
+    polls: {}
+  }
+
+  return {
+    initialState: initialState,
+
+    txHandler: (state, tx, chain) => {
+      if (tx.type === 'create') {
+        createHandler(state, tx, chain);
+      } else {
+        throw Error(`Unsupported tx type: ${tx}`);
+      }
+    },
+
+    blockHandler: blockHandler
+
+  }
+}
+
+module.exports = {
+  polls
+  initialState
+}
