1eb022888be54272deade6adf5b80689a61bee22
reid <reidsherman@gmail.com>
2018-04-07 17:42:38 -0700

    clean up

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -9,57 +9,6 @@ const createStore = () => {
       inactivePollIds: [],
       activePolls: {},
       inactivePolls: {},
-      // blocks: [
-      //   {
-      //     number: 1,
-      //     transactions: [],
-      //   },
-      //   {
-      //     number: 2,
-      //     transactions: [],
-      //   },
-      //   {
-      //     number: 3,
-      //     transactions: [],
-      //   },
-      //   {
-      //     number: 4,
-      //     transactions: [],
-      //   }
-      // ],
-      // polls: {
-      //   0: {
-      //     number: 0,
-      //     title: 'Do you prefer cats or dogs', //question
-      //     userVote: -1, 
-      //     hash: '0x8943djfF32mfasdf',
-      //     startBlock: 2,
-      //     endBlock: 9,
-      //     // getInput
-      //     // verifyVote
-      //     // inputReceived
-      //     // pollComplete
-      //     status: 'getInput',
-      //     optionReference: '0x98498453hjkd',
-      //     options: [
-      //       {
-      //         number: 0,
-      //         text: 'Cats',
-      //         img: '',
-      //         votes: 4,
-      //       },
-      //       {
-      //         number: 1,
-      //         text: 'Dogs',
-      //         img: '',
-      //         votes: 99,
-      //       },
-      //     ]
-      //   },
-      //   '0x4323249dsisf': {
-
-      //   }
-      // },
     },
     actions: {
       async getBlockchain ({ commit }) {
@@ -68,13 +17,11 @@ const createStore = () => {
       },
       async voteOnBlockchain ({ state, commit }, payload) {
         const pollId = payload.pollId
-        console.log(pollId)
         const poll = state.activePolls[pollId]
         if (poll) {
           // here we tell backend 
           // sign tx
           const publicKey = utils.privkeyToPubkey(utils.sha256('blah'))
-          console.log(publicKey)
           const tx = {
             type: "vote",
             question: poll.question,
@@ -84,9 +31,7 @@ const createStore = () => {
 
           const sigHash = utils.getTxHash(tx)
           tx.signature = utils.getSignature(sigHash, utils.sha256('blah'))
-          console.log('tx', tx)
           const vote = await this.$axios.$post('http://localhost:3001/txs', tx)
-          console.log('vote', vote)
           poll.userVote = payload.answer
           poll.status = 'voteReceived'
         }
